c19525133e7de895d76bdc92cf53c3e0ed5229df
Mohammed Abdul Qadeer <abdulqadeer82514@gmail.com>
2025-01-10 21:26:20 +0530

    pushing arrays folder

---
+++ src/ArraysDemo.java
@@ -0,0 +1,26 @@
+public class ArraysDemo {
+
+	public static void main(String[] args) {
+
+//		int arr[5];
+
+//		int arr[] = { 10, 20, 30, 40, 50 };
+
+		int arr[] = new int[5];
+
+		arr[0] = 10;
+		arr[1] = new Integer(20); // Autoboxing
+		arr[3] = 30;
+
+		System.out.println("elements of the array: ");
+
+//		for (int i = 0; i < arr.length; i++) {
+//			System.out.println(arr[i]);
+//		}
+
+		for (int element : arr) {
+			System.out.println(element);
+		}
+	}
+
+}
